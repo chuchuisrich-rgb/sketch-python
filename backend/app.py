ba4dd8f91b8dbc5d6a078fe5da6df296989bbb9d
chuchu <chuchuisrich@gmail.com>
2025-10-26 15:48:03 -0700

    fix log path to support it in render

--- backend/app.py
+++ backend/app.py
@@ -5,9 +5,10 @@ from utils import setup_logging
 import routes
 
 app = Flask(__name__)
-setup_logging("/Users/aravind/growth/chrome-plugin/summarizer_full_app_logs", "summarizer_app.log")
+setup_logging("logs", "summarizer_app.log")
 
 routes.init_app(app)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
